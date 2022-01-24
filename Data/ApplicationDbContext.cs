1ee783ecf62607b05a93cdcde46fbaa7fc4dfdc8
Dragan Pepic <nedimco2006@gmail.com>
2022-01-24 09:59:15 +0100

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using CourseManagement.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseManagement.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Course>().HasKey(c => c.Id);
+            builder.Entity<Course>().Ignore(c => c.StartingDate);
+            builder.Entity<Course>().Ignore(c => c.EndingDate);
+        }
+    }
+}
---
+++ Models/Course.cs
@@ -0,0 +1,29 @@
+﻿namespace CourseManagement.Models
+{
+    public class Course
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Title { get; set; } = string.Empty;
+
+        public TimeSpan Duration { get; set; }
+        public TimeOnly StartingTime { get; set; }
+        public TimeOnly EndingTime { get => StartingTime.AddHours(Duration.TotalHours); }
+
+        public DateOnly StartingDate
+        {
+            get => DateOnly.Parse(StartingDateDB);
+            set => StartingDateDB = value.ToString();
+        }
+
+        public string StartingDateDB { set; get; } = string.Empty;
+
+        public DateOnly EndingDate
+        {
+            get => DateOnly.Parse(EndingDateDB);
+            set => EndingDateDB = value.ToString();
+        }
+
+        public string EndingDateDB { set; get; } = string.Empty;
+        public List<DayOfWeek> DayOfWeek { get; set; } = new();
+    }
+}
